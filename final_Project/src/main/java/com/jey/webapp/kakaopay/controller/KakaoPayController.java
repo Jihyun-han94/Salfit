fcef4eaf68592141a25788ccb683bc24869a33cd
한지현 <gkswlgus923@gamil.com>
2021-07-09 16:11:42 +0900

    오류 수정

--- final_Project/src/main/java/com/jey/webapp/kakaopay/controller/KakaoPayController.java
+++ final_Project/src/main/java/com/jey/webapp/kakaopay/controller/KakaoPayController.java
@@ -35,163 +35,176 @@ import com.jey.webapp.product.service.ProductService;
 @Controller
 @RequestMapping(value="/pay")
 public class KakaoPayController {
-	
-	@Autowired
-	private CartService cart;
-	@Autowired	
-	private OrderService order;
-	@Autowired	
-	private AccountService account;
-	@Autowired
-	private ProductService product;
-	@Autowired
-	private AlertHandler alerthandler;
-	
-	@RequestMapping(value="", method = RequestMethod.GET)
-	public String payment(Model m, HttpServletRequest request,@ModelAttribute CartDTO dto) throws Exception {
-		
-		String[] id = request.getParameterValues("id");
-		int cartNum = 0;
-		List<CartDTO> cartlist = new ArrayList<CartDTO>();
-		int price =0;
-		List<Integer> price_arr = new ArrayList<Integer>();
-		int sumMoney =0;
-		int totalMoney = 0;
-		int delfee = 0; //배송비
-		
-		HttpSession session = request.getSession();
-		//계정 정보 확인
-		AccountDTO accountdto = (AccountDTO) session.getAttribute("account");
-		
-		List<AccountAddressDTO> address_arr =account.getList(accountdto.getId());
-		
-	
-		for(int i=0;i<id.length;i++){
-		 cartNum = Integer.parseInt(id[i]);
-		 dto.setId(cartNum);
-		 dto = cart.find(dto);
-		 cart.updateOrderState(dto);
-		
-		 cartlist.add(dto);
-		 price_arr.add(dto.getMoney());
-		}
-		
-		for(int i : price_arr) {
-			sumMoney =sumMoney + i;
-		}
-		
-		if(sumMoney >=30000) {
-			delfee=0;
-			totalMoney = sumMoney;
-			m.addAttribute("totalMoney", totalMoney);
-			m.addAttribute("delfee",delfee);
-			
-		}else {
-			delfee = 3000;
-			totalMoney = sumMoney +delfee;
-			m.addAttribute("totalMoney",totalMoney);
-			m.addAttribute("delfee",delfee);
-		}
-		
-		m.addAttribute("cartlist",cartlist);
-		m.addAttribute("sumMoney", sumMoney);
-		m.addAttribute("address_arr", address_arr);
-		
-		
-		String forward  = "kakaopay/payment";
-		
-		return forward;
-	}
-	@RequestMapping(value="", method = RequestMethod.POST )
-	
-	public String getpayment( @ModelAttribute CartDTO dto,@ModelAttribute OrderDTO order_dto,Model m) throws Exception {
-		String forward="kakaopay/paystep1";
-		
-		int aid = dto.getAid();
-		String receiver = order_dto.getReceiver();
-		String address = order_dto.getAddress();
-		int total = order_dto.getTotal();
-		order_dto.setAid(aid);
-		
-		List<CartDTO>cartlist = cart.yfindAll(dto); //장바구니에 담은 제품만 조회
-		order_dto.setDdate(cart.findDdate(dto));	// 가장 빠른 배송일 
-		order_dto.setEdate(cart.findEdate(dto));	// 가장 늦은 배송일
-		
-		order_dto.setPaytype("card");
-		boolean result = order.add(order_dto); //ordered table에 insert
-		
-		//ordered table 셋팅 
-		order_dto = order.selectone(order_dto);
-		
-		OrderDetailDTO orderdetail_dto = new OrderDetailDTO();
-		
-		for(CartDTO data:cartlist) {
-			orderdetail_dto.setOid(order_dto.getId());
-			orderdetail_dto.setPid(data.getPid());
-			orderdetail_dto.setPrice(data.getPrice());
-			orderdetail_dto.setQty(data.getQty());
-			orderdetail_dto.setStartdate(data.getStartdate());
-			orderdetail_dto.setEnddate(data.getEnddate());
-			orderdetail_dto.setDays(data.getDays());
-			
-			//order_detail table 셋팅
-			result = order.addDetail(orderdetail_dto);
-		}
-			//cart에서 삭제하는 method
-			boolean deleteresult = cart.delete(dto);
-			
-			
-			// 주문알림 
-			List<WebSocketSession>sList = this.alerthandler.sockList;
-			for(WebSocketSession ws: sList) {
-				String text = "새로운 주문이 들어왔습니다.";
-				TextMessage msg = new TextMessage(text);
-				ws.sendMessage(msg);
-			}
-			
-			
-			m.addAttribute("ordered", order_dto);
-		
-		return forward;
-	}
-	
-	@RequestMapping(value="confirm", method = RequestMethod.GET)
-	public ModelAndView confirmpayment(Model m, HttpServletRequest request,@ModelAttribute OrderDTO dto) throws Exception {
-		
-		ModelAndView mv = new ModelAndView();
+   
+   @Autowired
+   private CartService cart;
+   @Autowired   
+   private OrderService order;
+   @Autowired   
+   private AccountService account;
+   @Autowired
+   private ProductService product;
+   @Autowired
+   private AlertHandler alerthandler;
+   
+   @RequestMapping(value="", method = RequestMethod.GET)
+   public String payment(Model m, HttpServletRequest request,@ModelAttribute CartDTO dto) throws Exception {
+      
+      String[] id = request.getParameterValues("id");
+      int cartNum = 0;
+      List<CartDTO> cartlist = new ArrayList<CartDTO>();
+      int price =0;
+      List<Integer> price_arr = new ArrayList<Integer>();
+      int sumMoney =0;
+      int totalMoney = 0;
+      int delfee = 0; //배송비
+      
+      HttpSession session = request.getSession();
+      //계정 정보 확인
+      AccountDTO accountdto = (AccountDTO) session.getAttribute("account");
+      
+      List<AccountAddressDTO> address_arr =account.getList(accountdto.getId());
+      
+   
+      for(int i=0;i<id.length;i++){
+       cartNum = Integer.parseInt(id[i]);
+       dto.setId(cartNum);
+       dto = cart.find(dto);
+      
+       cartlist.add(dto);
+       price_arr.add(dto.getMoney());
+      }
+      
+      for(int i : price_arr) {
+         sumMoney =sumMoney + i;
+      }
+      
+      if(sumMoney >=30000) {
+         delfee=0;
+         totalMoney = sumMoney;
+         m.addAttribute("totalMoney", totalMoney);
+         m.addAttribute("delfee",delfee);
+         
+      }else {
+         delfee = 3000;
+         totalMoney = sumMoney +delfee;
+         m.addAttribute("totalMoney",totalMoney);
+         m.addAttribute("delfee",delfee);
+      }
+      
+      m.addAttribute("cartlist",cartlist);
+      m.addAttribute("sumMoney", sumMoney);
+      m.addAttribute("address_arr", address_arr);
+      
+      
+      String forward  = "kakaopay/payment";
+      
+      return forward;
+   }
+   @RequestMapping(value="", method = RequestMethod.POST )
+   
+   public String getpayment( @ModelAttribute CartDTO dto,@ModelAttribute OrderDTO order_dto,Model m,HttpServletRequest request) throws Exception {
+      String forward="kakaopay/paystep1";
+      
+      
+      
+      int aid = dto.getAid();
+      String receiver = order_dto.getReceiver();
+      String address = order_dto.getAddress();
+      int total = order_dto.getTotal();
+      order_dto.setAid(aid);
+      
 
-		HttpSession session = request.getSession();
-		//계정 정보 확인
-		AccountDTO accountdto = (AccountDTO) session.getAttribute("account");
-		
-	
-		String paymethod = request.getParameter("paymethod");
-		System.out.println(paymethod);
-		
-		//ordered table과 order_detail status 바꿔야됨!!
-		dto.setAid(accountdto.getId());
-		order.updatestatus(dto); //ordered status 'paid'로 변경
-		OrderDetailDTO detailDTO = new OrderDetailDTO();
-		detailDTO.setOid(dto.getId());
-		order.update(detailDTO); // order_detail status 'paid'로 변경
-		
-		// 결제 정보 Model m 으로 paystep2.jsp 에 전달하기!!
-		dto = order.findorder(dto);
-		
-		ProductDTO productdto = new ProductDTO();
-		//결제 완료시 product bcnt 1 증가
-		List<OrderDetailDTO> detail_arr = order.selectall(detailDTO);
-		for(OrderDetailDTO data:detail_arr) {
-			productdto.setId(data.getPid());
-			product.updatebcnt(productdto);
-		
-		}
-		m.addAttribute("paymethod", paymethod);
-		m.addAttribute("orderDTO", dto);
-		m.addAttribute("username", accountdto.getName());
-		mv.setViewName("/kakaopay/paystep2");
-		
-		return mv;
-	}
-	
+      int cartNum = 0;
+      String[] id = request.getParameterValues("cartid");
+      for(int i=0;i<id.length;i++){
+         cartNum = Integer.parseInt(id[i]);
+          dto.setId(cartNum);
+          dto = cart.find(dto);
+          cart.updateOrderState(dto);
+      }
+      
+       
+      List<CartDTO>cartlist = cart.yfindAll(dto); //장바구니에 담은 제품만 조회
+      order_dto.setDdate(cart.findDdate(dto));   // 가장 빠른 배송일 
+      order_dto.setEdate(cart.findEdate(dto));   // 가장 늦은 배송일
+      
+      order_dto.setPaytype("card");
+      boolean result = order.add(order_dto); //ordered table에 insert
+      
+      //ordered table 셋팅 
+      order_dto = order.selectone(order_dto);
+      
+      OrderDetailDTO orderdetail_dto = new OrderDetailDTO();
+      
+      for(CartDTO data:cartlist) {
+      
+         orderdetail_dto.setOid(order_dto.getId());
+         orderdetail_dto.setPid(data.getPid());
+         orderdetail_dto.setPrice(data.getPrice());
+         orderdetail_dto.setQty(data.getQty());
+         orderdetail_dto.setStartdate(data.getStartdate());
+         orderdetail_dto.setEnddate(data.getEnddate());
+         orderdetail_dto.setDays(data.getDays());
+         
+         //order_detail table 셋팅
+         result = order.addDetail(orderdetail_dto);
+      }
+         //cart에서 삭제하는 method
+         boolean deleteresult = cart.delete(dto);
+         
+         
+         // 주문알림 
+         List<WebSocketSession>sList = this.alerthandler.sockList;
+         for(WebSocketSession ws: sList) {
+            String text = "새로운 주문이 들어왔습니다.";
+            TextMessage msg = new TextMessage(text);
+            ws.sendMessage(msg);
+         }
+         
+         
+         m.addAttribute("ordered", order_dto);
+      
+      return forward;
+   }
+   
+   @RequestMapping(value="confirm", method = RequestMethod.GET)
+   public ModelAndView confirmpayment(Model m, HttpServletRequest request,@ModelAttribute OrderDTO dto) throws Exception {
+      
+      ModelAndView mv = new ModelAndView();
+
+      HttpSession session = request.getSession();
+      //계정 정보 확인
+      AccountDTO accountdto = (AccountDTO) session.getAttribute("account");
+      
+   
+      String paymethod = request.getParameter("paymethod");
+      System.out.println(paymethod);
+      
+      //ordered table과 order_detail status 바꿔야됨!!
+      dto.setAid(accountdto.getId());
+      order.updatestatus(dto); //ordered status 'paid'로 변경
+      OrderDetailDTO detailDTO = new OrderDetailDTO();
+      detailDTO.setOid(dto.getId());
+      order.update(detailDTO); // order_detail status 'paid'로 변경
+      
+      // 결제 정보 Model m 으로 paystep2.jsp 에 전달하기!!
+      dto = order.findorder(dto);
+      
+      ProductDTO productdto = new ProductDTO();
+      //결제 완료시 product bcnt 1 증가
+      List<OrderDetailDTO> detail_arr = order.selectall(detailDTO);
+      for(OrderDetailDTO data:detail_arr) {
+         productdto.setId(data.getPid());
+         product.updatebcnt(productdto);
+      
+      }
+      m.addAttribute("paymethod", paymethod);
+      m.addAttribute("orderDTO", dto);
+      m.addAttribute("username", accountdto.getName());
+      mv.setViewName("/kakaopay/paystep2");
+      
+      return mv;
+   }
+   
 }
--- final_Project/src/main/webapp/WEB-INF/views/kakaopay/payment.jsp
+++ final_Project/src/main/webapp/WEB-INF/views/kakaopay/payment.jsp
@@ -29,20 +29,20 @@
   color: #ffffff;
 }
 th, tr, th, table {
-	border-right: #ffff;
-	border-left: #ffff;
-	border-top: #e1e1e1;
-	border-bottom: #e1e1e1;
-	padding-right: 30px;
-	padding-left: 30px;
-	padding-bottom: 20px;
-	padding-top: 20px;
+   border-right: #ffff;
+   border-left: #ffff;
+   border-top: #e1e1e1;
+   border-bottom: #e1e1e1;
+   padding-right: 30px;
+   padding-left: 30px;
+   padding-bottom: 20px;
+   padding-top: 20px;
 }
 </style>
 </head>
 <body>
 <header style="padding-bottom:150px;">
-	<jsp:include page="/WEB-INF/views/module/top_nav.jsp"></jsp:include>
+   <jsp:include page="/WEB-INF/views/module/top_nav.jsp"></jsp:include>
 </header>
 <main class="cd-main-content sub-nav">
 <div style="margin: 0 auto; width:63%; font-family : 'Kakao', 'sans-serif', 'normal'; font-size:13px;">
@@ -50,68 +50,68 @@ th, tr, th, table {
 <form action="${buy }" method="post">
 <div style="float: center; padding-right:5px;">
 <h1 style="margin-bottom: 50px;"><i class="bi bi-caret-right-fill" style="color:#e1e1e1; font-size: 20px;"> </i>주문 내역 확인</h1>
-	<table border="1" id="id_cart" name="id_cart" style="text-align:center;">
-		<th style="border-top: solid 5px #e1e1e1; padding-bottom:10px;">이미지</th>
-		<th style="border-top: solid 5px #e1e1e1; padding-bottom:10px;">상품 명</th>
-		<th style="border-top: solid 5px #e1e1e1; padding-bottom:10px;">가격</th>
-		<th style="border-top: solid 5px #e1e1e1; padding-bottom:10px;">배송 방법</th>
-		<th style="border-top: solid 5px #e1e1e1; padding-bottom:10px;">구독일 (기간)</th>
-		<th style="border-top: solid 5px #e1e1e1; padding-bottom:10px;">총 주문 수량</th>
-		<th style="border-top: solid 5px #e1e1e1; padding-bottom:10px;">금액</th>
-		
-		<c:forEach var="data" items="${requestScope.cartlist }">
-		<tr>
-		
-		<td><a href="${detail}?id=${data.getPid()}">
-			<img class="rounded card-img-top productImg" style="object-fit:contain;
+   <table border="1" id="id_cart" name="id_cart" style="text-align:center;">
+      <th style="border-top: solid 5px #e1e1e1; padding-bottom:10px;">이미지</th>
+      <th style="border-top: solid 5px #e1e1e1; padding-bottom:10px;">상품 명</th>
+      <th style="border-top: solid 5px #e1e1e1; padding-bottom:10px;">가격</th>
+      <th style="border-top: solid 5px #e1e1e1; padding-bottom:10px;">배송 방법</th>
+      <th style="border-top: solid 5px #e1e1e1; padding-bottom:10px;">구독일 (기간)</th>
+      <th style="border-top: solid 5px #e1e1e1; padding-bottom:10px;">총 주문 수량</th>
+      <th style="border-top: solid 5px #e1e1e1; padding-bottom:10px;">금액</th>
+      
+      <c:forEach var="data" items="${requestScope.cartlist }">
+      <tr>
+      
+      <td><a href="${detail}?id=${data.getPid()}">
+         <img class="rounded card-img-top productImg" style="object-fit:contain;
             width:100px;
             height:100px;"
-				src="${pageContext.request.contextPath}${data.getUrl()}" 
-				>
-		</a></td>
-		<td>${data.title }</td>
-		<td>${data.price }</td>
-		<td>택배</td>
-		<td>${data.startdate } ~ ${data.enddate } (${data.days }일간)</td>
-		<td>${data.qty * data.days}</td>
-		<td>${data.money }</td>
-		</tr>
-		
-		</c:forEach>
-	</table>
-	</div>
-		<p style="color: gray; padding-top:5px;">* 상품 금액 30000원 이하는 배송비가 추가됩니다.</p>
-	<div style="float:right; padding-bottom: 50px; padding-right:50px;">
-		<label style="border-top: #e1e1e1; padding-bottom:10px;">총 주문 금액 : </label>
-		<label style="border-top: #e1e1e1; padding-top:10px; padding-bottom:13px; font-size: 20px; color: #961533; font-weight:bold;"><input name="total" value="${totalMoney }" hidden>${totalMoney } 원</label>
-	</div>
-	<div style="float:left; padding-top:100px;">
-		<h1 style="margin-bottom: 50px;"><i class="bi bi-caret-right-fill" style="color:#e1e1e1; font-size: 20px;"> </i>배송 정보</h1>
-			<label for="receiver">받으시는 분 : </label>
-			<input id="receiver" name="receiver" required>
-		<br>
-			<label for="address">주소 : </label>
-			<select name="address" id="address">
-			<c:forEach var="data" items="${requestScope.address_arr }">
-  				<option>${data.address }</option>
-			</c:forEach>
-  			</select>
-		
+            src="${pageContext.request.contextPath}${data.getUrl()}" 
+            >
+      </a></td>
+      <td><input name="cartid" value="${data.id }" type="hidden">${data.title }</td>
+      <td>${data.price }</td>
+      <td>택배</td>
+      <td>${data.startdate } ~ ${data.enddate } (${data.days }일간)</td>
+      <td>${data.qty * data.days}</td>
+      <td>${data.money }</td>
+      </tr>
+      
+      </c:forEach>
+   </table>
+   </div>
+      <p style="color: gray; padding-top:5px;">* 상품 금액 30000원 이하는 배송비가 추가됩니다.</p>
+   <div style="float:right; padding-bottom: 50px; padding-right:50px;">
+      <label style="border-top: #e1e1e1; padding-bottom:10px;">총 주문 금액 : </label>
+      <label style="border-top: #e1e1e1; padding-top:10px; padding-bottom:13px; font-size: 20px; color: #961533; font-weight:bold;"><input name="total" value="${totalMoney }" hidden>${totalMoney } 원</label>
+   </div>
+   <div style="float:left; padding-top:100px;">
+      <h1 style="margin-bottom: 50px;"><i class="bi bi-caret-right-fill" style="color:#e1e1e1; font-size: 20px;"> </i>배송 정보</h1>
+         <label for="receiver">받으시는 분 : </label>
+         <input id="receiver" name="receiver" required>
+      <br>
+         <label for="address">주소 : </label>
+         <select name="address" id="address">
+         <c:forEach var="data" items="${requestScope.address_arr }">
+              <option>${data.address }</option>
+         </c:forEach>
+           </select>
+      
 
-		<br>
-			<c:forEach var="data" items="${requestScope.cartlist }">
-				<input id="aid" name="aid" value="${data.aid }" hidden />
-			</c:forEach>
-			<input id="paytype" name="paytype" value="card" hidden />
-			<div style="text-align:center; margin-top:50px; margin-left: 400px;">	
-			<button type="submit" class="buy_btn1">결제</button>
-		</div>
-	</div>
-	
-	
+      <br>
+         <c:forEach var="data" items="${requestScope.cartlist }">
+            <input id="aid" name="aid" value="${data.aid }" hidden />
+         </c:forEach>
+         <input id="paytype" name="paytype" value="card" hidden />
+         <div style="text-align:center; margin-top:50px; margin-left: 400px;">   
+         <button type="submit" class="buy_btn1">결제</button>
+      </div>
+   </div>
+   
+   
 </form>
 </div>
 </main>
-<jsp:include page="/WEB-INF/views/module/footer.jsp"></jsp:include>	
+<jsp:include page="/WEB-INF/views/module/footer.jsp"></jsp:include>   
 </body>
 </html>
